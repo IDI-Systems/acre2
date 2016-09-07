f6415249fba1e579ed26060e828b55fcacd4acd5
jonpas <jonpas33@gmail.com>
2016-09-07 18:02:52 +0200

    Enable parallel compilation

--- tools/make.py
+++ tools/make.py
@@ -340,26 +340,26 @@ def compile_extensions(extensions_root, force_build):
         joinstr = ":rebuild;" if force_build else ";"
 
         # Prepare build dirs
-        vcproj = os.path.join(extensions_root,"vcproj")
+        vcproj32 = os.path.join(extensions_root,"vcproj")
         vcproj64 = os.path.join(extensions_root,"vcproj64")
-        if not os.path.exists(vcproj):
-            os.mkdir(vcproj)
+        if not os.path.exists(vcproj32):
+            os.mkdir(vcproj32)
         if not os.path.exists(vcproj64):
             os.mkdir(vcproj64)
 
         # 32-bit
-        os.chdir(vcproj)
+        os.chdir(vcproj32)
         subprocess.call(["cmake", "..", "-DUSE_64BIT_BUILD=OFF", "-G", "Visual Studio 14 2015"])
         print()
         extensions32_cmd = joinstr.join(extensions32)
-        subprocess.call(["msbuild", "ACRE.sln", "/t:{}".format(extensions32_cmd), "/p:Configuration=RelWithDebInfo"])
+        subprocess.call(["msbuild", "ACRE.sln", "/m", "/t:{}".format(extensions32_cmd), "/p:Configuration=RelWithDebInfo"])
 
         # 64-bit
         os.chdir(vcproj64)
         subprocess.call(["cmake", "..", "-DUSE_64BIT_BUILD=ON", "-G", "Visual Studio 14 2015 Win64"])
         print()
         extensions64_cmd = joinstr.join(extensions64)
-        subprocess.call(["msbuild", "ACRE.sln", "/t:{}".format(extensions64_cmd), "/p:Configuration=RelWithDebInfo"])
+        subprocess.call(["msbuild", "ACRE.sln", "/m", "/t:{}".format(extensions64_cmd), "/p:Configuration=RelWithDebInfo"])
     except:
         print_error("COMPILING EXTENSIONS.")
         raise
