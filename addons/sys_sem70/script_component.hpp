5fda05ea8101ee038c38a65e281cc539b6bfdf0a
JP <soldia@gmx.de>
2016-10-11 23:21:50 +0200

    Script_Component Harmonization

--- addons/sys_sem70/script_component.hpp
+++ addons/sys_sem70/script_component.hpp
@@ -1,6 +1,10 @@
 #define COMPONENT sys_sem70
-
+#define COMPONENT_BEAUTIFIED SEM 70
 #include "\idi\acre\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
 
 #define DEBUG_ENABLED_SYS_SEM70
 #ifdef DEBUG_ENABLED_SYS_SEM70
@@ -11,14 +15,7 @@
     #define DEBUG_SETTINGS DEBUG_SETTINGS_SYS_SEM70
 #endif
 
-
 #include "\idi\acre\addons\main\script_macros.hpp"
-
-//#define GET_STATE(id)            ([GVAR(currentRadioId), "getState", id] call acre_sys_data_fnc_dataEvent)
-//#define SET_STATE(id, val)        ([GVAR(currentRadioId), "setState", [id, val]] call acre_sys_data_fnc_dataEvent)
-//#define SET_STATE_CRIT(id, val)    ([GVAR(currentRadioId), "setStateCritical", [id, val]] call acre_sys_data_fnc_dataEvent)
-
-//#define GET_STATE_DEF(id, default)    ([id, default] call FUNC(getDefaultState))
 
 #define GET_VAR(var1) acre_player getVariable QUOTE(GVAR(var1))
 #define SET_VAR(var1,var2) acre_player setVariable [QUOTE(GVAR(var1)), var2]
