0e2e9596a3a0a5d4b3fa85fadd0b8136b80623e1
jonpas <jonpas33@gmail.com>
2016-10-19 20:19:23 +0200

    Update required CBA version to 3.1.1

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -11,7 +11,7 @@
 
 // MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 1.64
-#define REQUIRED_CBA_VERSION {3,1,0}
+#define REQUIRED_CBA_VERSION {3,1,1}
 
 #ifdef COMPONENT_BEAUTIFIED
     #define COMPONENT_NAME QUOTE(ACRE2 - COMPONENT_BEAUTIFIED)
