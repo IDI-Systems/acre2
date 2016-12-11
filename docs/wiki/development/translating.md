5718d91a999d2a41844f6a68a52ba2ec9ac11ca1
jonpas <jonpas33@gmail.com>
2016-12-11 15:38:20 +0100

    Add translation tracking to Travis

---
+++ docs/wiki/development/translating.md
@@ -0,0 +1,28 @@
+---
+title: Translating
+---
+
+## Guide
+
+This page describes in short how you can help translating ACRE2 into your language.
+
+1. If you don't already have a GitHub account create one at [http://github.com](http://github.com)
+2. Open the ACRE2 repository at [https://github.com/IDI-Systems/acre2](https://github.com/IDI-Systems/acre2)
+3. In the top right corner press the `Fork` button
+4. You are being redirected to your personal fork, a version that is hosted on your GitHub account (it says `yourUsername/acre2`)
+5. Clone that fork to your PC (There are a lot of tools and tutorials how to do this (e.g. [GitHub for Windows](https://windows.github.com)))
+6. Download [tabler](https://github.com/bux578/tabler/releases) and extract it somewhere
+7. Open the `tabler.exe` and in the menu click `File > Open language files`
+8. Point tabler to the cloned ACRE2 directory
+9. Start translating
+10. When done go to the menu and click `File > Save language files`
+11. Commit your changes to your local cloned repository
+12. Sync (Push) your commit(s) to your fork
+13. Create a Pull Request for your changes
+
+(The last three steps require some git knowledge, but there are a lot of tutorials out there)
+[GitHub for Windows help](https://windows.github.com/help.html)
+
+### Community Translation Guides
+
+**Spanish:** [https://gist.github.com/Legolasindar/bf8a3b09cb835f72501f](https://gist.github.com/Legolasindar/bf8a3b09cb835f72501f)
---
+++ tools/deploy.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+#####################################
+# ACRE2 automatic deployment script #
+# ================================= #
+# This is not meant to be run       #
+# directly!                         #
+#####################################
+
+import os
+import sys
+import shutil
+import traceback
+import subprocess as sp
+from github import Github, InputGitAuthor
+
+
+TRANSLATIONISSUE = 51
+TRANSLATIONBODY = """**[ACRE2 Translation Guide](http://acre2.idi-systems.com/wiki/development/translating)**
+
+{}
+"""
+
+REPOUSER = "IDI-Systems"
+REPONAME = "acre2"
+REPOPATH = "{}/{}".format(REPOUSER,REPONAME)
+
+
+def update_translations(repo):
+    diag = sp.check_output(["python3", "tools/stringtablediag.py", "--markdown"])
+    diag = str(diag, "utf-8")
+    issue = repo.get_issue(TRANSLATIONISSUE)
+    issue.edit(body=TRANSLATIONBODY.format(diag))
+
+
+def main():
+    print("Obtaining token ...")
+    try:
+        token = os.environ["GH_TOKEN"]
+        repo = Github(token).get_repo(REPOPATH)
+    except:
+        print("Could not obtain token.")
+        print(traceback.format_exc())
+        return 1
+    else:
+        print("Token sucessfully obtained.")
+
+    print("\nUpdating translation issue ...")
+    try:
+        update_translations(repo)
+    except:
+        print("Failed to update translation issue.")
+        print(traceback.format_exc())
+        return 1
+    else:
+        print("Translation issue successfully updated.")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
