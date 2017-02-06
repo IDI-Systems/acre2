92437f9e9a9c182f691fe733230bd013e31fd6cc
JP <soldia@gmx.de>
2017-02-06 19:59:22 +0100

    Init Stuff

--- addons/sys_sem70/CfgEventhandlers.hpp
+++ addons/sys_sem70/CfgEventhandlers.hpp
@@ -1,10 +1,11 @@
 class Extended_PreInit_EventHandlers {
     class ADDON    {
-        init = QUOTE(call COMPILE_FILE(XEH_pre_init));
+        init = QUOTE(call COMPILE_FILE(XEH_preInit));
     };
 };
+
 class Extended_PostInit_EventHandlers {
     class ADDON    {
-        clientInit = QUOTE(call COMPILE_FILE(XEH_post_init));
+        clientInit = QUOTE(call COMPILE_FILE(XEH_postInit));
     };
 };
