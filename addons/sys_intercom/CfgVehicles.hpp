fee8eef966fb5b5ab51a16996cb8d4746c192f17
TheMagnetar <magnetar5891@gmail.com>
2017-03-05 23:47:16 +0100

    Redefined infantry phone position for compatibility with other mods

--- addons/sys_intercom/CfgVehicles.hpp
+++ addons/sys_intercom/CfgVehicles.hpp
@@ -30,36 +30,74 @@ class CfgVehicles {
         acre_hasIntercom = 1;
         acre_hasInfantryPhone = 1;
     };
-    class Tank_F;
-    class MBT_01_base_F: Tank_F {
+
+    // BLUFOR
+    class MBT_01_base_F;
+    class B_MBT_01_base_F: MBT_01_base_F {};
+
+    class B_MBT_01_cannon_F: B_MBT_01_base_F {
         acre_infantryPhonePosition[] = {1.35, -4.4, -1};
     };
-    class MBT_01_arty_base_F: MBT_01_base_F {
+
+    class MBT_01_arty_base_F: MBT_01_base_F {};
+    class B_MBT_01_arty_base_F: MBT_01_arty_base_F {};
+
+    class B_MBT_01_arty_F: B_MBT_01_arty_base_F {
         acre_infantryPhonePosition[] = {1.35, -4.86, -1.4};
     };
-    class MBT_01_mlrs_base_F: MBT_01_base_F {
+
+    class MBT_01_mlrs_base_F: MBT_01_base_F {};
+    class B_MBT_01_mlrs_base_F: MBT_01_mlrs_base_F {};
+
+    class B_MBT_01_mlrs_F: B_MBT_01_mlrs_base_F {
         acre_infantryPhonePosition[] = {1.35, -4.43, -0.33};
     };
-    class APC_Tracked_01_base_F: Tank_F {
+
+    class B_APC_Tracked_01_base_F;
+    class B_APC_Tracked_01_rcws_F: B_APC_Tracked_01_base_F {
         acre_infantryPhonePosition[] = {-1.1, -4.86, -0.82};
     };
-    class B_APC_Tracked_01_base_F;
+
     class B_APC_Tracked_01_AA_F: B_APC_Tracked_01_base_F {
         acre_infantryPhonePosition[] = {-1.1, -4.85, -1.14};
     };
-    class MBT_02_base_F: Tank_F {
+
+    class B_APC_Tracked_01_CRV_F: B_APC_Tracked_01_base_F {
+        acre_infantryPhonePosition[] = {-1.1, -4.86, -0.82};
+    };
+
+    // OPFOR
+    class MBT_02_base_F;
+    class O_MBT_02_base_F: MBT_02_base_F {};
+
+    class O_MBT_02_cannon_F: O_MBT_02_base_F {
         acre_infantryPhonePosition[] = {1.38, -4.77, -1.1};
     };
-    class MBT_02_arty_base_F: MBT_02_base_F {
+
+    class MBT_02_arty_base_F: MBT_02_base_F {};
+    class O_MBT_02_arty_base_F: MBT_02_arty_base_F {};
+
+    class O_MBT_02_arty_F: O_MBT_02_arty_base_F {
         acre_infantryPhonePosition[] = {1.4, -5.4, -1.65};
     };
-    class APC_Tracked_02_base_F: Tank_F {
+
+    class O_APC_Tracked_02_base_F;
+    class O_APC_Tracked_02_cannon_F: O_APC_Tracked_02_base_F {
         acre_infantryPhonePosition[] = {0.98, -4.9, -0.79};
     };
-    class MBT_03_base_F: Tank_F {
+
+    class O_APC_Tracked_02_AA_F: O_APC_Tracked_02_base_F {
+        acre_infantryPhonePosition[] = {0.98, -4.9, -0.79};
+    };
+
+    // INDEPENDENT
+    class I_MBT_03_base_F;
+    class I_MBT_03_cannon_F: I_MBT_03_base_F {
         acre_infantryPhonePosition[] = {1.53, -5.67, -1.29};
     };
-    class APC_Tracked_03_base_F: Tank_F {
+
+    class I_APC_tracked_03_base_F;
+    class I_APC_tracked_03_cannon_F: I_APC_tracked_03_base_F {
         acre_infantryPhonePosition[] = {1.1, -3.87, -0.78};
     };
 
