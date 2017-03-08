8613f7e03191671e911453fa673b549ba3ecfdde
Soldia1138 <soldia@gmx.de>
2017-03-08 21:36:55 +0100

    SEM70 (#199)
    Adds a new radio: SEM70, a manpack radio used by German Armed Forces.
    
    It comes with an UI, rendered in 3D like all other ACRE2 radios. Model and textures created by Raspu (https://github.com/Raspu86).  
    Additionally it is the first radio featuring an automatic channel selection. Details will follow in the documentation.

---
+++ addons/sys_sem70/CfgAcreRadios.hpp
@@ -0,0 +1,88 @@
+#include "script_component.hpp"
+
+class CfgAcreComponents {
+    class ACRE_BaseRadio;
+
+    class ACRE_SEM70_base: ACRE_BaseRadio {
+        class Interfaces;
+        isAcre = 1;
+    };
+
+    class ACRE_SEM70: ACRE_SEM70_base {
+        name = "SEM 70";
+        sinadRating = -117;
+        sensitivityMin = -117;
+        sensitivityMax = -50;
+        isPackRadio = 1;
+        isDeployable = 0;
+
+        connectors[] =     {
+            {"Antenna", ACRE_CONNECTOR_BNC},
+            {"Audio/Data", ACRE_CONNECTOR_U_283}
+        };
+        defaultComponents[] = {
+            {0, "ACRE_SEM70_LONG_BNC"}
+        };
+
+        class InterfaceClasses {
+            CfgAcreDataInterface = "DefaultRadioInterface";
+            CfgAcreInteractInterface = "DefaultRadioInterface";
+            CfgAcreTransmissionInterface = "DefaultRadioInterface";
+            CfgAcrePhysicalInterface = "DefaultRadioInterface";
+        };
+
+        class Interfaces: Interfaces {
+            class CfgAcreDataInterface {
+                getListInfo = QFUNC(getListInfo);
+
+                setVolume = QFUNC(setVolume); // [0-1]
+                getVolume = QFUNC(getVolume); // [] = 0-1
+
+                setSpatial = QFUNC(setSpatial);
+                getSpatial = QFUNC(getSpatial);
+
+                setChannelData = QFUNC(setChannelData); // [channelNumber, [channelData] ]
+                getChannelData = QFUNC(getChannelData); // [channelNumber] = channelData
+                getCurrentChannelData = QFUNC(getCurrentChannelData); // channelData (of current channel)
+
+
+                getCurrentChannel = QFUNC(getCurrentChannel); // [] = channelNumber
+                setCurrentChannel = QFUNC(setCurrentChannel); // [channelNumber]
+
+                getStates = QFUNC(getStates); // [] = [ [stateName, stateData], [stateName, stateData] ]
+                getState = QFUNC(getState); // [stateName] = stateData
+                setState = QFUNC(setState); // [stateName, stateData] = sets state
+                setStateCritical = QFUNC(setState); // [stateName, stateData] = sets state
+
+                getOnOffState = QFUNC(getOnOffState); // [] = 0/1
+                setOnOffState = QFUNC(setOnOffState); // [ZeroOrOne]
+
+                initializeComponent = QFUNC(initializeRadio);
+
+                getChannelDescription = QFUNC(getChannelDescription);
+
+                isExternalAudio = QFUNC(isExternalAudio);
+            };
+
+            class CfgAcrePhysicalInterface {
+                getExternalAudioPosition = QFUNC(getExternalAudioPosition);
+            };
+
+            class CfgAcreTransmissionInterface {
+                handleBeginTransmission = QFUNC(handleBeginTransmission);
+                handleEndTransmission = QFUNC(handleEndTransmission);
+
+                handleSignalData = QFUNC(handleSignalData);
+                handleMultipleTransmissions =    QFUNC(handleMultipleTransmissions);
+
+                handlePTTDown = QFUNC(handlePTTDown);
+                handlePTTUp = QFUNC(handlePTTUp);
+            };
+
+            class CfgAcreInteractInterface {
+                openGui = QFUNC(openGui);                // [RadioId]
+                closeGui = QFUNC(closeGui);                // []
+            };
+        };
+    };
+};
---
+++ addons/sys_sem70/functions/fnc_onVolumeKnobTurn.sqf
@@ -0,0 +1,43 @@
+/*
+ * Author: ACRE2Team
+ * SHORT DESCRIPTION
+ *
+ * Arguments:
+ * 0: ARGUMENT ONE <TYPE>
+ * 1: ARGUMENT TWO <TYPE>
+ *
+ * Return Value:
+ * RETURN VALUE <TYPE>
+ *
+ * Example:
+ * [ARGUMENTS] call acre_COMPONENT_fnc_FUNCTIONNAME
+ *
+ * Public: No
+ */
+#include "script_component.hpp"
+
+params ["","_key"];
+
+private _currentDirection = -1;
+if (_key == 0) then {
+    // left click
+    _currentDirection = 1;
+};
+
+private _knobPosition = ["getState", "volumeKnobPosition"] call GUI_DATA_EVENT;
+
+// Channel selected do Volume control
+private _newKnobPosition = ((_knobPosition + _currentDirection) max 0) min 5;
+
+if (_knobPosition != _newKnobPosition) then {
+
+    ["setState", ["volumeKnobPosition",_newKnobPosition]] call GUI_DATA_EVENT;
+
+    //private _currentVolume = GET_STATE("volume"); //["getState", "volume"] call GUI_DATA_EVENT;
+    private _newVolume = abs ((_newKnobPosition)/5);
+    ["setVolume", _newVolume] call GUI_DATA_EVENT;
+
+    ["Acre_SEMKnob", [0,0,0], [0,0,0], 0.3, false] call EFUNC(sys_sounds,playSound);
+};
+
+[MAIN_DISPLAY] call FUNC(render);
