d3780b81cf8503dfc99a9dfd44924ebf3e303699
Tim Beswick <contact.tim.here@gmail.com>
2017-03-15 14:28:54 +0000

    Teamspeak channel switching functionality (#225)
    * Initial commit for channel move functionality
    
    * Changes as per previous review
    
    * Request changes and improvements
    
    * Fixed incorrect use of EGVAR
    
    * Requested changes. Normalised name to TS3 and normalised formatting.
    
    * Improved name search utilising Levenshtein distance
    
    * Whitespace
    
    * Requested changes
    
    * Whitespaceroni
    
    * Requested changes
    
    * Removed comment from pipe timeout
    
    * Use display XEH instead of PFH
    
    * Add missing checks in display XEH

---
+++ extensions/src/ACRE2Core/setServerName.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "IRpcFunction.h"
+#include "Log.h"
+#include "TextMessage.h"
+#include "Engine.h"
+
+RPC_FUNCTION(setServerName) {
+    std::string name;
+    name = std::string((char *) vMessage->getParameter(0));
+
+    CEngine::getInstance()->getClient()->updateServerName(name);
+    return ACRE_OK;
+}
+DECLARE_MEMBER(char *, Name);
+};
