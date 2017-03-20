2079f57f5f576e77c87a3dce48c4fa201816badf
TheMagnetar <magnetar5891@gmail.com>
2017-03-20 11:32:15 +0100

    Added box to units[] in CfgPatches

--- addons/sys_radio/config.cpp
+++ addons/sys_radio/config.cpp
@@ -3,7 +3,7 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {"ACRE_DeployedRadioBase"};
+        units[] = {"ACRE_DeployedRadioBase", "ACRE_radioSupplyCrate"};
         weapons[] = {"ACRE_BaseRadio"};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"acre_main", "acre_sys_core", "acre_sys_antenna", "acre_sys_components", "acre_sys_data", "acre_sys_sounds"};
