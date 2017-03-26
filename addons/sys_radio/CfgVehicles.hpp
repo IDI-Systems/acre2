7f256a3b69115e02e78b073e066525964ba96851
jonpas <jonpas33@gmail.com>
2017-03-26 19:38:16 +0200

    Rename tag, lower amount of radios, more small radios than big radios

--- addons/sys_radio/CfgVehicles.hpp
+++ addons/sys_radio/CfgVehicles.hpp
@@ -1,8 +1,6 @@
-#define NUM_RADIOS_IN_CRATE 50
-
 class CfgVehicles {
     class NATO_Box_Base;
-    class ACRE_radioSupplyCrate: NATO_Box_Base {
+    class ACRE_RadioSupplyCrate: NATO_Box_Base {
         scope = 2;
         scopeCurator = 2;
         accuracy = 1000;
@@ -10,13 +8,13 @@ class CfgVehicles {
         model = "\A3\weapons_F\AmmoBoxes\AmmoBox_F";
         author = ECSTRING(main,Author);
         class TransportItems {
-            MACRO_ADDITEM(ACRE_PRC77,NUM_RADIOS_IN_CRATE);
-            MACRO_ADDITEM(ACRE_PRC117F,NUM_RADIOS_IN_CRATE);
-            MACRO_ADDITEM(ACRE_PRC148,NUM_RADIOS_IN_CRATE);
-            MACRO_ADDITEM(ACRE_PRC152,NUM_RADIOS_IN_CRATE);
-            MACRO_ADDITEM(ACRE_PRC343,NUM_RADIOS_IN_CRATE);
-            MACRO_ADDITEM(ACRE_SEM52SL,NUM_RADIOS_IN_CRATE);
-            MACRO_ADDITEM(ACRE_SEM70,NUM_RADIOS_IN_CRATE);
+            MACRO_ADDITEM(ACRE_PRC77,5);
+            MACRO_ADDITEM(ACRE_PRC117F,5);
+            MACRO_ADDITEM(ACRE_PRC148,10);
+            MACRO_ADDITEM(ACRE_PRC152,10);
+            MACRO_ADDITEM(ACRE_PRC343,10);
+            MACRO_ADDITEM(ACRE_SEM52SL,10);
+            MACRO_ADDITEM(ACRE_SEM70,5);
         };
     };
 };
