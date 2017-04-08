1aaa06c1dad9b8450018b6babcabfe01da1d2717
jonpas <jonpas@users.noreply.github.com>
2017-04-09 01:08:16 +0200

    Fix distortion test plugin and map gen compilation (#280)
    * Update DistortionTestPlugin with ACRE2TS3 additions (fixes compile errors), Fix mapgen compile errors
    
    * Add proper omnidirectional settings support to mapgen
    
    * Bool instead of float
    
    * Get bool from int properly
    
    * Use passed value for processing, Less redundant comparison

--- extensions/src/ACRE2DistortionTestPlugin/TS3Client.h
+++ extensions/src/ACRE2DistortionTestPlugin/TS3Client.h
@@ -4,6 +4,8 @@
 #include "TsFunctions.h"
 #include <thread>
 #include <string>
+#include <vector>
+typedef std::vector<std::string> Sentence;
 
 class CTS3Client: public IClient {
 public:
@@ -34,7 +36,8 @@ public:
     ACRE_RESULT localStopSpeaking( ACRE_SPEAKING_TYPE speakingType );
 
     std::string getTempFilePath( void );
-
+    std::string getConfigFilePath(void);
+    
     ACRE_RESULT playSound(std::string path, ACRE_VECTOR position, float volume, int looping);
 
     std::string getUniqueId( );
@@ -44,6 +47,16 @@ public:
     ACRE_RESULT microphoneOpen(BOOL status);
 
     ACRE_RESULT unMuteAll( void );
+
+    ACRE_RESULT moveToServerTS3Channel();
+    ACRE_RESULT moveToPreviousTS3Channel();
+    uint64 findChannelByName(std::string name);
+    unsigned int levenshteinDistance(const Sentence& string1, const Sentence& string2);
+    Sentence split(const std::string &s, char delim, Sentence &elems);
+    Sentence split(const std::string &s, char delim);
+    ACRE_RESULT updateServerName(std::string name);
+    ACRE_RESULT updateShouldSwitchTS3Channel(BOOL state);
+    BOOL shouldSwitchTS3Channel();
 
     DECLARE_MEMBER(BOOL, hadVAD);
     DECLARE_MEMBER(BOOL, InputActive);
@@ -55,7 +68,10 @@ public:
     DECLARE_MEMBER(BOOL, DirectFirst);
     DECLARE_MEMBER(BOOL, HitTSSpeakingEvent);
     DECLARE_MEMBER(BOOL, IsX3DInitialized);
-    DECLARE_MEMBER(UINT32, SpeakerMask)
+    DECLARE_MEMBER(UINT32, SpeakerMask);
+    DECLARE_MEMBER(uint64, PreviousTSChannel);
+    DECLARE_MEMBER(std::string, ServerName);
+    DECLARE_MEMBER(BOOL, ShouldSwitchTS3Channel)
 protected:
     std::thread m_versionThreadHandle;
     char *m_vadLevel;
