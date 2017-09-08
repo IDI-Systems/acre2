58b4c72efb937963199e30290cefd4e38c9739de
jonpas <jonpas33@gmail.com>
2017-09-08 04:37:53 +0200

    Fix Publish script version when merge fails

--- tools/jenkins.py
+++ tools/jenkins.py
@@ -89,15 +89,20 @@ do_action(["git", "checkout", current_branch], "Failed to checkout back into che
 do_action(make_args, "Make failed")
 
 version = get_project_version("..\\addons\\\main\\script_version.hpp")
-commit_message = "v{}.{}.{}.{} - Build {}".format(version[0],version[1],version[2],version[3],os.environ["BUILD_NUMBER"])
+version_str = "{}.{}.{}.{}".format(version[0],version[1],version[2],version[3])
+commit_message = "v{} - Build {}".format(version_str,os.environ["BUILD_NUMBER"])
+
 do_action(["git", "commit", "-am", commit_message], "Failed to commit changes back into branch '{}'".format(current_branch))
 do_action(["git", "push", "origin", current_branch], "Failed to push changes back into branch 'origin/{}'".format(current_branch))
 do_action(["git", "checkout", target_branch], "Failed to checkout target branch '{}'".format(target_branch))
 do_action(["git", "pull", "origin", target_branch], "Failed to update target branch from 'origin/{}'".format(target_branch))
+
 status_ok = do_action(["git", "merge", current_branch], "Failed to merge '{}' into '{}', conflict exists.".format(current_branch, target_branch), create_pull_request, [repository, current_branch, target_branch, github_token], True)
 if(status_ok): # Only diff and push if merge was successful
     do_action(["git", "diff"], "Diff failed to resolve '{}' and '{}' cleanly, conflict exists.".format(current_branch, target_branch))
     do_action(["git", "push", "origin", target_branch], "Failed to push changes back into branch 'origin/{}'".format(target_branch))
-do_action(["python", "-u", "publish.py", "..\\manifest.json", "-r", release_target], "Publish failed.")
+
+# Pass version in case merge failed above (publish.py would try to read pre-merge file)
+do_action(["python", "-u", "publish.py", "..\\manifest.json", "-r", release_target, "-v", version_str], "Publish failed.")
 
 sys.exit(0)
