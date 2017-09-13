ea5bdec26ee0278167e8a7aad1051ae1b5f681e7
jonpas <jonpas@users.noreply.github.com>
2017-09-13 04:43:36 +0200

    Use CBA Misc Items (#401)
    * Use CBA_MiscItem_ItemInfo (1.76 VA Mine Detectors fix 1/2)
    
    * Use CBA_MiscItem (1.76 VA Mine Detector fix 2/2)
    
    * Require CBA 3.4.1

--- addons/sys_prc117f/CfgWeapons.hpp
+++ addons/sys_prc117f/CfgWeapons.hpp
@@ -1,7 +1,6 @@
 class CfgWeapons {
-    class Default;
     class ACRE_BaseRadio;
-    class ItemCore;
+    class CBA_MiscItem_ItemInfo;
 
     class ACRE_PRC117F: ACRE_BaseRadio {
         displayName = "AN/PRC-117F";
@@ -11,15 +10,12 @@ class CfgWeapons {
         descriptionShort = "AN/PRC-117F Manpack Radio";
         scopeCurator = 2;
         scope = 2;
-        class ItemInfo {
-             mass = 120;
-             allowedSlots[] = {901};
-            type = 0;
+
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 120;
+            allowedSlots[] = {901};
             scope = 0;
          };
-
-        type = 4096;
-        simulation = "ItemMineDetector";
 
         class Library {
             libTextDesc = "AN/PRC-117F Manpack Radio";
