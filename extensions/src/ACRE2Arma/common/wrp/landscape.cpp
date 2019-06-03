0fe72a5680085c0a3564992a6872a3cec6c624e6
Magnetar <magnetar5891@gmail.com>
2019-06-03 21:46:49 +0200

    Cleanup. Remove compile warnings. New  program to generate fakewrp

--- extensions/src/ACRE2Arma/common/wrp/landscape.cpp
+++ extensions/src/ACRE2Arma/common/wrp/landscape.cpp
@@ -1,54 +1,67 @@
 #include "landscape.hpp"
 #include <exception>
 
-acre::wrp::landscape::landscape(std::string path_)
-{
+acre::wrp::landscape::landscape(const std::string &path_) {
     std::ifstream stream(path_, std::ifstream::binary);
     _process(stream);
     stream.close();
 }
 
-acre::wrp::landscape::landscape(std::istream &stream_)
-{
+acre::wrp::landscape::landscape(std::istream &stream_) {
     _process(stream_);
 }
 
-acre::wrp::landscape::~landscape()
-{
+acre::wrp::LandscapeResult acre::wrp::landscape::parse(const std::string &path_) {
+    std::ifstream stream(path_, std::ifstream::binary);
+    _process(stream);
+    stream.close();
+
+    return failure;
 }
 
-bool acre::wrp::landscape::_read_binary_tree_block(std::istream &stream_, uint32_t bit_length, uint32_t block_size, uint32_t cell_size, uint32_t block_offset_x, uint32_t block_offset_y)
+acre::wrp::LandscapeResult acre::wrp::landscape::parse(std::istream &stream_) {
+    _process(stream_);
+
+    return failure;
+}
+
+acre::wrp::landscape::~landscape() {
+}
+
+bool acre::wrp::landscape::_read_binary_tree_block(std::istream &stream_, const uint32_t bit_length, const uint32_t block_size, const uint32_t cell_size, const uint32_t block_offset_x, const uint32_t block_offset_y)
 {
-    block_size = (int)ceil((log10(block_size) / log10(2)) / 2);
-    block_size = (int)pow(2, 2 * block_size);
+    uint32_t blockSize = static_cast<uint32_t>(ceil((log10(block_size) / log10(2.0)) / 2.0));
+    blockSize = static_cast<uint32_t>(pow(2, 2.0 * blockSize));
 
-    uint32_t current_block_size = block_size / 4;//256..64..16..1
-    uint32_t current_block_offset_x, current_block_offset_y;
+    const uint32_t current_block_size = blockSize / 4u;//256..64..16..1
 
-    uint16_t A, B;
-    uint16_t packet_flag;
+    uint16_t packet_flag = 0;
     stream_.read((char *)&packet_flag, sizeof(uint16_t));
 
-    for (int bit_count = 0; bit_count < 16; bit_count++, packet_flag >>= 1)
-    {
-        current_block_offset_x = block_offset_x + (bit_count % 4) * block_size*bit_length;// 0,1,2,3
-        current_block_offset_y = block_offset_y + ((bit_count / 4) * block_size); // 0,1,2,3
+    for (int32_t bit_count = 0; bit_count < 16; bit_count++, packet_flag >>= 1) {
+        const uint32_t current_block_offset_x = block_offset_x + (bit_count % 4) * blockSize*bit_length;// 0,1,2,3
+        const uint32_t current_block_offset_y = block_offset_y + ((bit_count / 4) * blockSize); // 0,1,2,3
 
-        if (packet_flag & 1)
-        {
-            if (!_read_binary_tree_block(stream_, bit_length, current_block_size, cell_size, current_block_offset_x, current_block_offset_y)) return false;
-        }
-        else
-        {
+        if (packet_flag & 1) {
+            if (!_read_binary_tree_block(stream_, bit_length, current_block_size, cell_size, current_block_offset_x, current_block_offset_y)) {
+                return false;
+            }
+        } else {
+            uint16_t A = 0, B = 0;
             stream_.read((char *)&A, bit_length);
             stream_.read((char *)&B, bit_length);
-            if (!A && !B)     continue;// filler
-            if ((current_block_offset_x >= cell_size) || (current_block_offset_y >= cell_size)) return false;
+            if (!A && !B) {
+                continue; // filler
+            }
 
-            for (uint32_t iy = 0; iy < current_block_offset_y; iy++) {
-                for (uint32_t ix = 0; ix < current_block_size; ix++)
+            if ((current_block_offset_x >= cell_size) || (current_block_offset_y >= cell_size)) {
+                return false;
+            }
+
+            for (uint32_t iy = 0u; iy < current_block_offset_y; iy++) {
+                for (uint32_t ix = 0u; ix < current_block_size; ix++)
                 {
-                    int OffsetAB = ((current_block_offset_y + iy)*cell_size) + current_block_offset_x + ix;
+                    const int32_t OffsetAB = ((current_block_offset_y + iy)*cell_size) + current_block_offset_x + ix;
                     //_Grid[OffsetAB] = A; _Grid[OffsetAB + ThisBlockSize.x] = B;
                 }
             }
@@ -57,11 +70,10 @@ bool acre::wrp::landscape::_read_binary_tree_block(std::istream &stream_, uint32
     return true;
 }
 
-bool acre::wrp::landscape::_process(std::istream &stream_)
-{
+bool acre::wrp::landscape::_process(std::istream &stream_) {
     stream_.read(filetype, 4);
     filetype[4] = 0x00;
-    failure = 0;
+    failure = LandscapeResult::Success;
 
     if (strcmp(filetype, "8WVR") == 0) {
         // Header
@@ -72,11 +84,11 @@ bool acre::wrp::landscape::_process(std::istream &stream_)
         stream_.read((char *)&map_size_x, sizeof(uint32_t));
         stream_.read((char *)&map_size_y, sizeof(uint32_t));
         //Cell size
-        stream_.read((char *)&cell_size, sizeof(float));
+        stream_.read((char *)&cell_size, sizeof(float32_t));
         map_grid_size = cell_size;
 
         // elevations (Elevations   [TerrainGridSize.y][TerrainGridSize.x];)
-        elevations = compressed<float>(stream_, map_size_x*map_size_x, false, false, version);
+        elevations = compressed<float32_t>(stream_, map_size_x * map_size_x, false, false, version);
 
         //Generate peaks.
         return true;
@@ -84,20 +96,21 @@ bool acre::wrp::landscape::_process(std::istream &stream_)
     
     // Proceed with OPRW/ACRE
     stream_.read((char *)&version, sizeof(uint32_t));
-    if (version > 24)
+    if (version > 24) {
         stream_.read((char *)&some_bit, sizeof(uint32_t));
+    }
     stream_.read((char *)&layer_size_x, sizeof(uint32_t));
     stream_.read((char *)&layer_size_y, sizeof(uint32_t));
     stream_.read((char *)&map_size_x, sizeof(uint32_t));
     stream_.read((char *)&map_size_y, sizeof(uint32_t));
 
-    stream_.read((char *)&cell_size, sizeof(float));
+    stream_.read((char *)&cell_size, sizeof(float32_t));
     map_grid_size = cell_size * layer_size_x / map_size_x;
-
-    uint8_t flag; // Determines if the blocks are present.
 
     // Failure is only likely on recompression/reading gridblocks.
     try {
+        uint8_t flag; // Determines if the blocks are present.
+
         /* GridBlock - CellEnv */
         stream_.read((char *)&flag, sizeof(uint8_t));
         if (flag == '\x1') {
@@ -121,11 +134,11 @@ bool acre::wrp::landscape::_process(std::istream &stream_)
         uint32_t peak_count;
         stream_.read((char *)&peak_count, sizeof(uint32_t));
 
-        float *peak_floats = new float[peak_count * 3];
-        stream_.read((char *)peak_floats, sizeof(float)*peak_count * 3);
+        float32_t *peak_floats = new float32_t[peak_count * 3u];
+        stream_.read((char *)peak_floats, sizeof(float32_t)*peak_count * 3u);
 
-        for (uint32_t i = 0; i < peak_count * 3; i = i + 3) {
-            peaks.push_back(vector3<float>(peak_floats[i], peak_floats[i + 1], peak_floats[i + 2]));
+        for (uint32_t i = 0u; i < peak_count * 3u; i = i + 3u) {
+            peaks.push_back(vector3<float32_t>(peak_floats[i], peak_floats[i + 1u], peak_floats[i + 2u]));
         }
         delete[] peak_floats;
 
@@ -141,7 +154,7 @@ bool acre::wrp::landscape::_process(std::istream &stream_)
 
         /* CHEEKY HACK for our dummy Tanoa WRP */
         if (strcmp(filetype, "ACRE") == 0) {
-            elevations = compressed<float>(stream_, map_size_x*map_size_x, false, false, version);
+            elevations = compressed<float32_t>(stream_, map_size_x * map_size_x, false, false, version);
         } else {
             //Random clutter
             if (version <= 20) {
@@ -153,20 +166,78 @@ bool acre::wrp::landscape::_process(std::istream &stream_)
                 compressed<uint8_t> test_demcompress2 = compressed<uint8_t>(stream_, map_size_x*map_size_y, true, false, version);
             }
 
-            elevations = compressed<float>(stream_, map_size_x*map_size_y, true, false, version);
+            elevations = compressed<float32_t>(stream_, map_size_x * map_size_y, true, false, version);
         }
     } catch (std::exception& e) {
         LOG(ERROR) << "WRP Parsing exception: " << e.what();
         //Insert 1 peak in the middle.
         peaks.clear();
-        peaks.push_back(vector3<float>((map_size_x*map_grid_size)/2, (map_size_y*map_grid_size)/2,0));
+        peaks.push_back(vector3<float32_t>((map_size_x * map_grid_size)/2.0f, (map_size_y * map_grid_size)/2.0f, 0.0f));
 
         // Fill elevation grid with 0 data.
-        for (uint32_t x = 0; x < (map_size_x*map_size_y); x++) {
+        for (uint32_t x = 0u; x < (map_size_x * map_size_y); x++) {
             elevations.data.push_back(0.0f);
         }
         //elevations.size = elevations
-        failure = -1;
+        failure = LandscapeResult::Recovered;
     }
     return true;
+}
+
+
+AcreResult acre::wrp::landscape::generateAcreWrp(std::ofstream &out) {
+
+    /*
+     * Following the file format https://community.bistudio.com/wiki/Wrp_File_Format_-_OPRWv17_to_24
+     * in order to be compatible with our parser.
+     */
+
+    // ===================== Header =====================
+    out.write("ACRE", 4);                                // Filetype
+    out.write((char *)&version, sizeof(uint32_t));       // Version
+
+    // Some dummy bits
+    uint32_t fill_bits = 0u;
+    out.write((char *)&fill_bits, sizeof(uint32_t));     // As of version 25
+
+    out.write((char *)&layer_size_x, sizeof(uint32_t));  // Layer  Size
+    out.write((char *)&layer_size_y, sizeof(uint32_t));
+
+    out.write((char *)&map_size_x, sizeof(uint32_t));    // Map Size
+    out.write((char *)&map_size_y, sizeof(uint32_t));
+
+    out.write((char *)&cell_size, sizeof(uint32_t));     // Layer Cell Size
+
+    // ===================== Grid Block Cell Environment =====================
+    uint8_t not_present = 0;
+    out.write((char *)&not_present, sizeof(uint8_t));    // In order to spare size, we do not fill anything
+    out.write((char *)&fill_bits, sizeof(uint32_t));
+
+    // ===================== Grid Block CfgEnvSounds =====================
+    out.write((char *)&not_present, sizeof(uint8_t));    // In order to spare size, we do not fill anything
+    out.write((char *)&fill_bits, sizeof(uint32_t));
+
+    // ===================== Peaks =====================
+    fill_bits = static_cast<uint32_t>(peaks.size());
+    out.write((char *)&fill_bits, sizeof(uint32_t));
+
+    for (auto peak : peaks) {
+        float32_t value = peak.x();
+        out.write((char *)&value, sizeof(float32_t));
+        value = peak.y();
+        out.write((char *)&value, sizeof(float32_t));
+        value = peak.z();
+        out.write((char *)&value, sizeof(float32_t));
+    }
+
+    // ===================== Grid Block Rvmat Layer Index =====================
+    out.write((char *)&not_present, sizeof(uint8_t));    // In order to spare size, we do not fill anything
+    out.write((char *)&fill_bits, sizeof(uint32_t));
+
+    // ===================== And now the ACRE hack: elevations =====================
+    for (uint32_t elevIdx = 0u; elevIdx < (map_size_x * map_size_y); elevIdx++) {
+        out.write((char *) &elevations[elevIdx], sizeof(float32_t));
+    }
+
+    return AcreResult::ok;
 }
