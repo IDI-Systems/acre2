e502f9566f90004e3a9f69439618455f57165f16
jonpas <jonpas33@gmail.com>
2019-06-16 22:28:35 +0200

    Test

--- tools/jenkins.py
+++ tools/jenkins.py
@@ -128,10 +128,11 @@ if args.publish:
     do_action(["git", "status"], "Debug 1 error")
     do_action(["git", "branch"], "Debug 2 error")
     do_action(["git", "log", "--oneline", "-n20"], "Debug 3 error")
-    do_action(["git", "config", "--global", "--list"], "Debug 4 error")
-    do_action(["git", "config", "--list"], "Debug 5 error")
 
-    do_action(["git", "pull", "origin", target_branch], "Failed to update target branch from 'origin/{}'".format(target_branch))
+    do_action(["git", "pull"], "Failed to update target branch from 'origin/{}'".format(target_branch))
+
+    # DEBUG
+    do_action(["git", "status"], "Debug 4 error")
 
     status_ok = do_action(["git", "merge", current_branch], "Failed to merge '{}' into '{}', conflict exists.".format(current_branch, target_branch), create_pull_request, [repository, current_branch, target_branch, github_token], True)
     if(status_ok): # Only diff and push if merge was successful
