58aa760fa0e9916bcfe710abe8801f8f5883530f
jonpas <jonpas33@gmail.com>
2019-09-10 17:42:45 +0200

    Use CBA Versioning System properly (#825)
    * Use CBA Versioning System properly
    
    * Update CBA include to CBA v3.12.2 + excessive concatenation cleanup

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -5,9 +5,10 @@
 
 #include "\idi\acre\addons\main\script_version.hpp"
 
-#define VERSION MAJOR.MINOR.PATCHLVL.BUILD
-#define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
-#define VERSION_PLUGIN MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION         MAJOR.MINOR
+#define VERSION_STR     MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR      MAJOR,MINOR,PATCHLVL,BUILD
+#define VERSION_PLUGIN  MAJOR.MINOR.PATCHLVL.BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 1.94
