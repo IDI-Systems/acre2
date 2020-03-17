4068b36d510cddb44fd1c54dc6d9ce9b7b0679ea
jonpas <jonpas33@gmail.com>
2020-03-17 14:26:43 +0100

    Update CfgFunctions.hpp

--- addons/api/CfgFunctions.hpp
+++ addons/api/CfgFunctions.hpp
@@ -27,7 +27,7 @@ class CfgFunctions {
             PATHTO_FNC(hasBaseRadio);
             PATHTO_FNC(getRadioByType);
 
-            PATHTO_FNC(getUnitLoadout);
+            PATHTO_FNC(filterUnitLoadout);
 
             PATHTO_FNC(setItemRadioReplacement);
             PATHTO_FNC(getDisplayName);
