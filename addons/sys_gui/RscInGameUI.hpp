ed7c77d8cc9960acabf512db63eae161addc3294
jonpas <jonpas33@gmail.com>
2020-09-17 12:43:35 +0200

    Use IGUI_BCG_COLOR

--- addons/sys_gui/RscInGameUI.hpp
+++ addons/sys_gui/RscInGameUI.hpp
@@ -7,12 +7,7 @@ class RscInGameUI {
             w = IGUI_GRID_STANCE_WAbs;
             onLoad = "uiNamespace setVariable ['ACRE_AntennaElevationInfo', _this select 0];";
             text = QPATHTOF(data\ui\stand_straight.paa);
-            colorBackground[] = {
-                "profileNamespace getVariable ['IGUI_BCG_RGB_R', 0]",
-                "profileNamespace getVariable ['IGUI_BCG_RGB_G', 1]",
-                "profileNamespace getVariable ['IGUI_BCG_RGB_B', 1]",
-                "profileNamespace getVariable ['IGUI_BCG_RGB_A', 0.8]"
-            };
+            colorBackground[] = IGUI_BCG_COLOR;
         };
     };
 };
