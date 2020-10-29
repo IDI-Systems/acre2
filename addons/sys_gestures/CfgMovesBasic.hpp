0e1878366e6d141ed0d4aada73f6a3ef768353f3
Cyruz143 <root.cyruz@gmail.com>
2020-10-29 12:21:35 +0000

    Missing semi colons

--- addons/sys_gestures/CfgMovesBasic.hpp
+++ addons/sys_gestures/CfgMovesBasic.hpp
@@ -2,10 +2,10 @@ class CfgMovesBasic {
     class DefaultDie;
     class ManActions {
         GVAR(helmet) = QGVAR(helmet);
-        GVAR(helmet_noADS) = QGVAR(helmet_noADS)
-        GVAR(vest) = QGVAR(vest)
-        GVAR(vest_noADS) = QGVAR(vest_noADS)
-        GVAR(stop) = QGVAR(stop)
+        GVAR(helmet_noADS) = QGVAR(helmet_noADS);
+        GVAR(vest) = QGVAR(vest);
+        GVAR(vest_noADS) = QGVAR(vest_noADS);
+        GVAR(stop) = QGVAR(stop);
     };
     class Actions {
         class Default;
