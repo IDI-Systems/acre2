bc158ee5a7637294b3caea3449c8abb20d377415
Freddo <f.falk@protonmail.com>
2020-12-14 11:01:36 +0100

    Change to use CBA_settings_fnc_set

--- addons/api/XEH_PREP.hpp
+++ addons/api/XEH_PREP.hpp
@@ -2,4 +2,4 @@
 PREP(mapChannelFieldName);
 
 // Obsolete
-OBSOLETE_SYS(FUNC(setItemRadioReplacement),{params ['_radioType']; EGVAR(sys_radio,defaultItemRadioType) = _radioType;});
+OBSOLETE_SYS(FUNC(setItemRadioReplacement),{params ['_radioType']; [QEGVAR(sys_radio,defaultItemRadioType), _radioType, 1, "mission"] call CBA_settings_fnc_set;});
