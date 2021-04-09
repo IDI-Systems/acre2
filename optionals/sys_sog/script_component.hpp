1d7b6328b4284bee410f862f5212782cb096f24c
jonpas <jonpas33@gmail.com>
2021-04-09 02:29:44 +0200

    Fix debug macros component name

--- optionals/sys_sog/script_component.hpp
+++ optionals/sys_sog/script_component.hpp
@@ -2,12 +2,12 @@
 #define COMPONENT_BEAUTIFIED S.O.G. Prairie Fire Compatibility
 #include "\idi\acre\addons\main\script_mod.hpp"
 
-#ifdef DEBUG_ENABLED_SYS_VN
+#ifdef DEBUG_ENABLED_SYS_SOG
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_ENABLED_SYS_VN
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_SYS_VN
+#ifdef DEBUG_ENABLED_SYS_SOG
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SYS_SOG
 #endif
 
 #include "\idi\acre\addons\main\script_macros.hpp"
