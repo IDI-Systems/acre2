659a97e3bad0d7b91bd097588944e4012585b3a8
Filip Maciejewski <veteran29@users.noreply.github.com>
2021-05-03 18:45:04 +0200

    Add CBA versioning check for SOG:PF

--- addons/main/CfgSettings.hpp
+++ addons/main/CfgSettings.hpp
@@ -4,6 +4,8 @@ class CfgSettings {
             class PREFIX {
                 class dependencies {
                     CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "(true)"};
+
+                    compat_sogpf[] = {"sys_sog", {VERSION_AR}, "isClass (configFile >> 'CfgPatches' >> 'data_f_vietnam')"};
                 };
             };
         };
