IDI-Systems/acre2

commit 169ae75cd17fdff4af5b005d846566a690c0d12e

Make event processing async by introducing event queue

extensions/src/ACRE2Mumble/MumbleEventLoop.cpp
@@ -1,0 +1,63 @@
+#include "MumbleEventLoop.h"
+
+namespace acre {
+
+    MumbleEventLoop& MumbleEventLoop::getInstance() {
+        static MumbleEventLoop loop;
+
+        return loop;
+    }
+
+    MumbleEventLoop::MumbleEventLoop() : m_thread(&MumbleEventLoop::run, this) {
+    }
+
+    void MumbleEventLoop::stop() {
+        {
+            std::lock_guard<std::mutex> guard(m_lock);
+            m_keepRunning = false;
+        }
+
+        m_waiter.notify_all();
+
+        // Wait until the worker thread has finished
+        m_thread.join();
+    }
+
+    void MumbleEventLoop::queue(const std::function<void()>& callable) {
+        {
+            std::lock_guard<std::mutex> guard(m_lock);
+
+            m_queuedFunctions.push_back(callable);
+        }
+
+        m_waiter.notify_all();
+    }
+
+    void MumbleEventLoop::queue(std::function<void()>&& callable) {
+        {
+            std::unique_lock<std::mutex> guard(m_lock);
+
+            m_queuedFunctions.push_back(std::move(callable));
+        }
+
+        m_waiter.notify_all();
+    }
+
+    void MumbleEventLoop::run() {
+        std::unique_lock<std::mutex> guard(m_lock);
+
+        while (m_keepRunning) {
+            // Process all pending events
+            while (!m_queuedFunctions.empty()) {
+                std::function<void()> currentFunc = std::move(m_queuedFunctions.front());
+                m_queuedFunctions.pop_front();
+
+                currentFunc();
+            }
+
+            // Wait for something to happen. Note that during the waiting the lock is released and
+            // re-aquired once the waiting ends
+            m_waiter.wait(guard);
+        }
+    }
+}

extensions/src/ACRE2Mumble/MumbleCallbacks_init.cpp
@@ -3,6 +3,7 @@
 #include "MumbleClient.h"
 #include "MumbleCommandServer.h"
 #include "MumbleFunctions.h"
+#include "MumbleEventLoop.h"
 #include "compat.h"
 #include "helpers.h"
 
@@ -47,36 +48,44 @@ mumble_error_t mumble_init(mumble_plugin_id_t id) {
 }
 
 void mumble_onServerSynchronized(mumble_connection_t connection) {
-    activeConnection = connection;
+    acre::MumbleEventLoop::getInstance().queue([connection]() {
+        activeConnection = connection;
 
-    // set ID on every new connection
-    acre::id_t clientId = 0;
-	// TODO: Missing error handling of API call
-    mumAPI.getLocalUserID(pluginID, activeConnection, (mumble_userid_t *) &clientId);
-    CEngine::getInstance()->getSelf()->setId(clientId);
-    CEngine::getInstance()->getExternalServer()->setId(clientId);
+        // set ID on every new connection
+        acre::id_t clientId = 0;
+        // TODO: Missing error handling of API call
+        mumAPI.getLocalUserID(pluginID, activeConnection, (mumble_userid_t*)&clientId);
+        CEngine::getInstance()->getSelf()->setId(clientId);
+        CEngine::getInstance()->getExternalServer()->setId(clientId);
 
-    // subscribe to all channels to receive event
-    if (CEngine::getInstance()->getClient()->getState() != acre::State::running) {
-        CEngine::getInstance()->getClient()->start(static_cast<acre::id_t>(clientId));
-    }
+        // subscribe to all channels to receive event
+        if (CEngine::getInstance()->getClient()->getState() != acre::State::running) {
+            CEngine::getInstance()->getClient()->start(static_cast<acre::id_t>(clientId));
+        }
+    });
 }
 
 void mumble_onServerDisconnected(mumble_connection_t connection) {
-    activeConnection = -1;
+    acre::MumbleEventLoop::getInstance().queue([connection]() {
+        activeConnection = -1;
 
-    if ((CEngine::getInstance()->getClient()->getState() != acre::State::stopped) &&
-        (CEngine::getInstance()->getClient()->getState() != acre::State::stopping)) {
-        CEngine::getInstance()->getClient()->stop();
-    }
+        if ((CEngine::getInstance()->getClient()->getState() != acre::State::stopped) &&
+            (CEngine::getInstance()->getClient()->getState() != acre::State::stopping)) {
+            CEngine::getInstance()->getClient()->stop();
+        }
+    });
 }
 
 void mumble_shutdown() {
-    if ((CEngine::getInstance()->getClient()->getState() != acre::State::stopped) &&
-        (CEngine::getInstance()->getClient()->getState() != acre::State::stopping)) {
-        CEngine::getInstance()->getClient()->stop();
-    }
-    CEngine::getInstance()->stop();
+    acre::MumbleEventLoop::getInstance().queue([]() {
+        if ((CEngine::getInstance()->getClient()->getState() != acre::State::stopped) &&
+            (CEngine::getInstance()->getClient()->getState() != acre::State::stopping)) {
+            CEngine::getInstance()->getClient()->stop();
+        }
+        CEngine::getInstance()->stop();
+    });
+
+    acre::MumbleEventLoop::getInstance().stop();
 }
 
 void mumble_releaseResource(const void *) {

extensions/src/ACRE2Mumble/MumbleCallbacks_pluginEvents.cpp
@@ -1,6 +1,7 @@
 #include "Engine.h"
 #include "Log.h"
 #include "MumbleFunctions.h"
+#include "MumbleEventLoop.h"
 #include "compat.h"
 
 //
@@ -8,8 +9,11 @@
 //
 bool mumble_onReceiveData(mumble_connection_t connection, mumble_userid_t sender, const uint8_t *data, size_t dataLength, const char *dataID) {
     if ((dataLength > 0U) && CEngine::getInstance()->getExternalServer()) {
-		// TODO: Cast is probably unnecessary
-        CEngine::getInstance()->getExternalServer()->handleMessage((unsigned char *) data, dataLength);
+        // Copy data to make sure it stays available in the async processing
+        std::string dataString(reinterpret_cast<const char *>(data), dataLength);
+        acre::MumbleEventLoop::getInstance().queue([dataString]() {
+            CEngine::getInstance()->getExternalServer()->handleMessage((unsigned char *)dataString.data(), dataString.size());
+        });
         return true;
     }
     return false;

extensions/src/ACRE2Mumble/MumbleCallbacks_speaking.cpp
@@ -2,6 +2,7 @@
 #include "Log.h"
 #include "Macros.h"
 #include "MumbleClient.h"
+#include "MumbleEventLoop.h"
 #include "Types.h"
 #include "compat.h"
 
@@ -9,68 +10,73 @@
 // Mumble Speaking callbacks
 //
 void mumble_onUserTalkingStateChanged(mumble_connection_t connection, mumble_userid_t userID, mumble_talking_state_t status) {
-    TRACE("mumble_onUserTalkingStateChanged ENTER: %d", status);
-    if (static_cast<acre::id_t>(userID) != CEngine::getInstance()->getSelf()->getId()) {
-        return;
-    }
-
-    if (CEngine::getInstance()->getClient()->getState() != acre::State::running) {
-        return;
-    }
-
-    if (!CEngine::getInstance()->getGameServer()) {
-        return;
-    }
-
-    if (CEngine::getInstance()->getState() != acre::State::running) {
-        return;
-    }
-
-    if (!CEngine::getInstance()->getGameServer()->getConnected()) {
-        return;
-    }
-
-    CEngine::getInstance()->getClient()->setSpeakingState(status);
-    if (CEngine::getInstance()->getSoundSystemOverride()) {
-        return;
-    }
-
-    if (CEngine::getInstance()->getClient()->getOnRadio()) {
-        if (CEngine::getInstance()->getClient()->getVAD()) {
+    acre::MumbleEventLoop::getInstance().queue([connection, userID, status]() {
+        TRACE("mumble_onUserTalkingStateChanged ENTER: %d", status);
+        if (static_cast<acre::id_t>(userID) != CEngine::getInstance()->getSelf()->getId()) {
             return;
         }
 
-        if (status == MUMBLE_TS_PASSIVE || status == MUMBLE_TS_INVALID) {
-            if ((!CEngine::getInstance()->getClient()->getRadioPTTDown())
-                  && (!CEngine::getInstance()->getClient()->getGodPTTDown())
-                  && (!CEngine::getInstance()->getClient()->getZeusPTTDown())) {
-                CEngine::getInstance()->getClient()->setOnRadio(false);
-            } else {
-                if (!CEngine::getInstance()->getClient()->getDirectFirst()) {
-                    CEngine::getInstance()->getClient()->microphoneOpen(true);
-                } else {
-                    CEngine::getInstance()->getClient()->setDirectFirst(false);
-                    if ((CEngine::getInstance()->getClient()->getRadioPTTDown())
-                          || (CEngine::getInstance()->getClient()->getGodPTTDown())
-                          || (CEngine::getInstance()->getClient()->getZeusPTTDown())) {
+        if (CEngine::getInstance()->getClient()->getState() != acre::State::running) {
+            return;
+        }
+
+        if (!CEngine::getInstance()->getGameServer()) {
+            return;
+        }
+
+        if (CEngine::getInstance()->getState() != acre::State::running) {
+            return;
+        }
+
+        if (!CEngine::getInstance()->getGameServer()->getConnected()) {
+            return;
+        }
+
+        CEngine::getInstance()->getClient()->setSpeakingState(status);
+        if (CEngine::getInstance()->getSoundSystemOverride()) {
+            return;
+        }
+
+        if (CEngine::getInstance()->getClient()->getOnRadio()) {
+            if (CEngine::getInstance()->getClient()->getVAD()) {
+                return;
+            }
+
+            if (status == MUMBLE_TS_PASSIVE || status == MUMBLE_TS_INVALID) {
+                if ((!CEngine::getInstance()->getClient()->getRadioPTTDown())
+                    && (!CEngine::getInstance()->getClient()->getGodPTTDown())
+                    && (!CEngine::getInstance()->getClient()->getZeusPTTDown())) {
+                    CEngine::getInstance()->getClient()->setOnRadio(false);
+                }
+                else {
+                    if (!CEngine::getInstance()->getClient()->getDirectFirst()) {
                         CEngine::getInstance()->getClient()->microphoneOpen(true);
+                    }
+                    else {
+                        CEngine::getInstance()->getClient()->setDirectFirst(false);
+                        if ((CEngine::getInstance()->getClient()->getRadioPTTDown())
+                            || (CEngine::getInstance()->getClient()->getGodPTTDown())
+                            || (CEngine::getInstance()->getClient()->getZeusPTTDown())) {
+                            CEngine::getInstance()->getClient()->microphoneOpen(true);
+                        }
                     }
                 }
             }
+
+            return;
         }
+        TRACE("enter: [%d],[%d]", userID, status);
 
-        return;
-    }
-    TRACE("enter: [%d],[%d]", userID, status);
-
-    if ((status != MUMBLE_TS_PASSIVE) && (status != MUMBLE_TS_INVALID)) {
-        CEngine::getInstance()->getClient()->setDirectFirst(true);
-        CEngine::getInstance()->getClient()->localStartSpeaking(acre::Speaking::direct);
-    } else {
-        CEngine::getInstance()->getClient()->setDirectFirst(false);
-        CEngine::getInstance()->getClient()->localStopSpeaking(acre::Speaking::direct);
-        CEngine::getInstance()->getClient()->setMainPTTDown(false);
-        CEngine::getInstance()->getClient()->setGodPTTDown(false);
-        CEngine::getInstance()->getClient()->setZeusPTTDown(false);
-    }
+        if ((status != MUMBLE_TS_PASSIVE) && (status != MUMBLE_TS_INVALID)) {
+            CEngine::getInstance()->getClient()->setDirectFirst(true);
+            CEngine::getInstance()->getClient()->localStartSpeaking(acre::Speaking::direct);
+        }
+        else {
+            CEngine::getInstance()->getClient()->setDirectFirst(false);
+            CEngine::getInstance()->getClient()->localStopSpeaking(acre::Speaking::direct);
+            CEngine::getInstance()->getClient()->setMainPTTDown(false);
+            CEngine::getInstance()->getClient()->setGodPTTDown(false);
+            CEngine::getInstance()->getClient()->setZeusPTTDown(false);
+        }
+    });
 }

extensions/src/ACRE2Mumble/MumbleEventLoop.h
@@ -1,0 +1,32 @@
+#pragma once
+
+#include <functional>
+#include <deque>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+
+namespace acre {
+
+    class MumbleEventLoop {
+    public:
+        MumbleEventLoop();
+
+        static MumbleEventLoop& getInstance();
+
+        void stop();
+
+        void queue(const std::function<void()>& callable);
+        void queue(std::function<void()>&& callable);
+
+    protected:
+        std::mutex m_lock;
+        std::condition_variable m_waiter;
+        bool m_keepRunning = true;
+        std::deque<std::function<void()>> m_queuedFunctions;
+        std::thread m_thread;
+
+        void run();
+    };
+
+}

commit b72aca68160f349820cb02f619f6201f5f6025cc

Fix deadlock in plugin
169ae75cd17fdff4af5b005d846566a690c0d12e introduced the MumbleEventLoop
class, which was supposed to make the processing of Mumble callbacks
asynchronous. However, the loop was keeping the lock when executing
queued callbacks and if such a callback was to use a Mumble API function
while Mumble's main thread was currently executing one of the plugin
callbacks within ACRE, this API call would block. If the active callback
now tried to queue a new event, this would block as well, since the
event queue was still holding the lock for processing that function.
Thus, a deadlock occured.

Given that the timing had to be right for this to happen, the deadlocks
were now much less common than they were before the introduction of the
event queue, but with a bit of patience, they could still be created.

The solution is to simply release the lock while executing the currently
processed function in the event loop. This allows simultaneous queueing
of new events withouot blocking.

extensions/src/ACRE2Mumble/MumbleEventLoop.cpp
@@ -52,7 +52,14 @@ namespace acre {
                 std::function<void()> currentFunc = std::move(m_queuedFunctions.front());
                 m_queuedFunctions.pop_front();
 
+                // Execute the current funtion. However, during the time this function is executing, there is no need to
+                // keep holding m_lock. By releasing it for that time, we allow further functions to be queued by other threads.
+                // This gets especially important if the called function will call a Mumble API function. This function will require
+                // to run in Mumble's main thread but if that thread is in turn waiting until a new event is queued (in one of the
+                // plugin callbacks in this plugin), we end up with a deadlock.
+                guard.unlock();
                 currentFunc();
+                guard.lock();
             }
 
             // Wait for something to happen. Note that during the waiting the lock is released and