a5fed1ed72083df2b37a889080a7f1240d006cf8
jonpas <jonpas33@gmail.com>
2021-11-08 20:43:08 +0100

    Update addons/main/CfgSettings.hpp

--- addons/main/CfgSettings.hpp
+++ addons/main/CfgSettings.hpp
@@ -6,7 +6,6 @@ class CfgSettings {
                     CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "(true)"};
 
                     compat_gm[] = {"acre_sys_gm", {VERSION_AR}, "isClass (configFile >> 'CfgPatches' >> 'gm_core')"};
-
                     compat_sogpf[] = {"acre_sys_sog", {VERSION_AR}, "isClass (configFile >> 'CfgPatches' >> 'data_f_vietnam')"};
                 };
             };
