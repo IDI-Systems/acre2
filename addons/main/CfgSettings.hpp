96c6afffbfbee054a033880625aa10cef489ff97
Björn Dahlgren <bjorn@dahlgren.io>
2021-11-08 21:14:44 +0100

    General - Display missing compatibility warning for Global Mobilization (#1162)
    Co-authored-by: jonpas <jonpas33@gmail.com>

--- addons/main/CfgSettings.hpp
+++ addons/main/CfgSettings.hpp
@@ -6,6 +6,7 @@ class CfgSettings {
                     CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "(true)"};
 
                     compat_csla[] = {"acre_sys_csla", {VERSION_AR}, "isClass (configFile >> 'CfgPatches' >> 'CSLA')"};
+                    compat_gm[] = {"acre_sys_gm", {VERSION_AR}, "isClass (configFile >> 'CfgPatches' >> 'gm_core')"};
                     compat_sogpf[] = {"acre_sys_sog", {VERSION_AR}, "isClass (configFile >> 'CfgPatches' >> 'data_f_vietnam')"};
                 };
             };
