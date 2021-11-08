5d731c23cf384434d40f36de1cb8f7c62a6358b4
Björn Dahlgren <bjorn@dahlgren.io>
2021-11-08 20:33:59 +0100

    Add dependency on acre_sys_gm when CfgPatches gm_core is detected

--- addons/main/CfgSettings.hpp
+++ addons/main/CfgSettings.hpp
@@ -5,6 +5,8 @@ class CfgSettings {
                 class dependencies {
                     CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "(true)"};
 
+                    compat_gm[] = {"acre_sys_gm", {VERSION_AR}, "isClass (configFile >> 'CfgPatches' >> 'gm_core')"};
+
                     compat_sogpf[] = {"acre_sys_sog", {VERSION_AR}, "isClass (configFile >> 'CfgPatches' >> 'data_f_vietnam')"};
                 };
             };
