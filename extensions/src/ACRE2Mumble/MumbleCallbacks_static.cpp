4e54964e96a4acc586c7af7e731481b1adc6b0ae
Robert Adam <dev@robert-adam.de>
2021-12-19 16:19:05 +0100

    Added missing static plugin info functions

--- extensions/src/ACRE2Mumble/MumbleCallbacks_static.cpp
+++ extensions/src/ACRE2Mumble/MumbleCallbacks_static.cpp
@@ -30,6 +30,28 @@ mumble_version_t mumble_getVersion() {
     return mumble_version_t{ACRE_VERSION_MAJOR, ACRE_VERSION_MINOR, ACRE_VERSION_SUBMINOR};
 }
 
+struct MumbleStringWrapper mumble_getAuthor() {
+    static const char* author = "ACRE2 developers";
+
+    MumbleStringWrapper wrapper;
+    wrapper.data = author;
+    wrapper.size = strlen(author);
+    wrapper.needsReleasing = false;
+
+    return wrapper;
+}
+
+struct MumbleStringWrapper mumble_getDescription() {
+    static const char* description = "ACRE2 is a full fledged communications suite for Arma 3, allowing realistic radio and voice communications.";
+
+    MumbleStringWrapper wrapper;
+    wrapper.data = description;
+    wrapper.size = strlen(description);
+    wrapper.needsReleasing = false;
+
+    return wrapper;
+}
+
 mumble_version_t mumble_getPluginFunctionsVersion() {
     return MUMBLE_PLUGIN_FUNCTIONS_VERSION;
 }
