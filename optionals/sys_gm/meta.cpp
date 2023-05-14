863a15f12eb4881dbce0726bfbc30c46d79e9b1c
jonpas <jonpas33@gmail.com>
2023-05-15 01:12:16 +0200

    Add human-readable compatibility metadata
    Co-authored-by: Björn Dahlgren <bjorn@dahlgren.io>

--- optionals/sys_gm/meta.cpp
+++ optionals/sys_gm/meta.cpp
@@ -1,2 +1,3 @@
 protocol = 1;
 publishedid = 1740990569;
+name = "ACRE2 Global Mobilization Compatibility";
--- optionals/sys_ws/meta.cpp
+++ optionals/sys_ws/meta.cpp
@@ -1,2 +1,3 @@
 protocol = 1;
 publishedid = 2657862776;
+name = "ACRE2 Western Sahara Compatibility";
