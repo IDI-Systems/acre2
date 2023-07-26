b4bf6de0a6958f795092dc180d381c978f92c9fe
Drofseh <Drofseh@users.noreply.github.com>
2023-07-25 18:36:04 -0700

    Add command tanks to cfgpatches unts

--- optionals/sys_spe/config.cpp
+++ optionals/sys_spe/config.cpp
@@ -3,7 +3,36 @@
 class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
-        units[] = {};
+        units[] = {
+            "SPE_M4A1_75_Command",
+            "SPE_M4A1_75_Command_DVL",
+            "SPE_FR_M4A1_75_Command",
+            "SPE_FR_M4A1_75_Command_DVL",
+            "SPE_M4A1_76_Command",
+            "SPE_M4A1_76_Command_DVL",
+            "SPE_FR_M4A1_76_Command",
+            "SPE_FR_M4A1_76_Command_DVL",
+            "SPE_M4A0_75_Early_Command",
+            "SPE_M4A0_75_Early_Command_DVL",
+            "SPE_M4A0_75_Command",
+            "SPE_M4A0_75_Command_DVL",
+            "SPE_FR_M4A0_75_Early_Command",
+            "SPE_FR_M4A0_75_Early_Command_DVL",
+            "SPE_FR_M4A0_75_mid_Command",
+            "SPE_FR_M4A0_75_mid_Command_DVL",
+            "SPE_PzBefWgIII_K",
+            "SPE_PzBefWgIII_K_DLV",
+            "SPE_ST_PzBefWgIII_K",
+            "SPE_ST_PzBefWgIII_K_DLV",
+            "SPE_PzBefWgIV",
+            "SPE_PzBefWgIV_DLV",
+            "SPE_ST_PzBefWgIV",
+            "SPE_ST_PzBefWgIV_DLV",
+            "SPE_PzBefWgVI",
+            "SPE_PzBefWgVI_DLV",
+            "SPE_ST_PzBefWgVI",
+            "SPE_ST_PzBefWgVI_DLV"
+        };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"acre_main","WW2_SPE_Assets_c_Misc_ZZZ_LastLoaded_c"};
