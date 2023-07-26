fcfd07291b6df4c7f12ebfa45f22725725db8e55
Drofseh <Drofseh@users.noreply.github.com>
2023-07-25 19:15:35 -0700

    update requiredAddons and authors

--- optionals/sys_spe/config.cpp
+++ optionals/sys_spe/config.cpp
@@ -35,9 +35,9 @@ class CfgPatches {
         };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"acre_main","WW2_SPE_Assets_c_Misc_ZZZ_LastLoaded_c"};
+        requiredAddons[] = {"acre_main", "acre_api", "WW2_SPE_Assets_c_Misc_ZZZ_LastLoaded_c"};
         author = ECSTRING(main,Author);
-        authors[] = {"Heavy Ordnance Works"};
+        authors[] = {"Heavy Ordnance Works","drosfeh"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
     };
