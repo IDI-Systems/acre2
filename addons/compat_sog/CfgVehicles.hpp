546698727dbbaa44f76ae3a8a0a62c005cb7154a
jonpas <jonpas33@gmail.com>
2023-09-07 16:10:41 +0200

    Merge vehicles subconfig to parent

--- addons/compat_sog/CfgVehicles.hpp
+++ addons/compat_sog/CfgVehicles.hpp
@@ -1,34 +1,3 @@
-#include "script_component.hpp"
-
-class CfgPatches {
-    class ADDON {
-        name = COMPONENT_NAME;
-        units[] = {};
-        weapons[] = {};
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"acre_main", "loadorder_f_vietnam"};
-        skipWhenMissingDependencies = 1;
-        author = ECSTRING(main,Author);
-        authors[] = {"Savage Game Design", "veteran29"};
-        url = ECSTRING(main,URL);
-        VERSION_CONFIG;
-    };
-};
-
-// TODO move to sys_attenuate
-class CfgSoundEffects {
-    class AttenuationsEffects {
-        class vn_tank_attenuation {
-            acreAttenuation = 0.7;
-            acreAttenuationTurnedOut = 0.25;
-        };
-        class vn_car_attenuation {
-            acreAttenuation = 0.5;
-            acreAttenuationTurnedOut = 0.25;
-        };
-    };
-};
-
 class CfgVehicles {
     class vn_armor_tank_base;
     class vn_armor_m48_base: vn_armor_tank_base {
--- addons/compat_sog/compat_sog_vehicles/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#include "\idi\acre\addons\compat_sog\script_component.hpp"
-
-#define SUBCOMPONENT vehicles
-#undef ADDON
-#define ADDON TRIPLES(PREFIX,COMPONENT,SUBCOMPONENT)
