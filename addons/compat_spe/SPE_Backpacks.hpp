cb9c208923b22fbc11ae9875e73c54feb6a4626c
Drofseh <Drofseh@users.noreply.github.com>
2024-12-12 04:28:15 -0800

    Update Spearhead 1944 (SPE) compatibility, Add SPEX compat (#1335)

--- addons/compat_spe/SPE_Backpacks.hpp
+++ addons/compat_spe/SPE_Backpacks.hpp
@@ -19,3 +19,25 @@ class B_SPE_GER_Radio: B_SPE_AssaultPack_Base {
         };
     };
 };
+
+/*
+class B_SPE_GER_Radio_battery: B_SPE_GER_Radio {
+    //This is just a battery, no radio, remove class TransportItems to prevent inheriting ACRE_PRC77?
+};
+*/
+
+//Vichy France
+class B_SPE_Milice_Radio: B_SPE_AssaultPack_Base {
+    class TransportItems {
+        class _xx_ACRE_PRC77 {
+            count = 1;
+            name = "ACRE_PRC77";
+        };
+    };
+};
+
+/*
+class B_SPE_Milice_Radio_battery: B_SPE_Milice_Radio {
+    //This is just a battery, no radio, remove class TransportItems to prevent inheriting ACRE_PRC77?
+};
+*/
